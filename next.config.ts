d2bdc3b9407afe0c955ebaf516b5cc46d8ed0ca7
ibraSanusi <isanu227@gmail.com>
2025-05-20 23:31:50 +0200

    feature: rewrite

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/pages/dashboard",
+      },
+    ];
+  },
+};
+
 export default nextConfig;
