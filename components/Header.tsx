90ac38d1ce176d7a02319e12ddb5adb88eef96a7
ibraSanusi <isanu227@gmail.com>
2025-05-21 20:09:53 +0200

    feature: header dinamico

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { clsx } from "clsx";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <header
       className={clsx(
-        "flex w-full items-center justify-between bg-transparent p-4",
+        "fixed inset-x-0 top-0 z-10 flex items-center justify-between p-4 mt-4 max-w-7xl mx-auto transition-colors duration-300 rounded-xl",
+        scrolled
+          ? "bg-transparent/80 backdrop-blur-md outline outline-accent rounded-xl "
+          : "bg-transparent",
       )}
     >
       <Link href={"/"} className="text-2xl leading-none font-bold">
