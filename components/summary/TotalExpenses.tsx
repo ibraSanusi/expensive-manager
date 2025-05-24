cb8d771fc519fca9a07e3146c39b239c7e682af9
ibraSanusi <isanu227@gmail.com>
2025-05-24 23:45:30 +0200

    feature: se muestran los gastos del mes en curso

--- components/summary/TotalExpenses.tsx
+++ components/summary/TotalExpenses.tsx
@@ -9,22 +9,38 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Progress } from "../ui/progress";
+import { useTransactionStore } from "@/lib/store/useTransactionStore";
+import { getMonthlyExpenses } from "@/lib/helpers/transactions";
 
+const MONEY_TO_EXPENSE = 1200; // Presupuesto mensual
+
+/**
+ * Componente que muestra los gastos totales del mes actual.
+ * Utiliza el store de transacciones para obtener los datos necesarios.
+ */
 export function TotalExpenses() {
+  const { transactions } = useTransactionStore();
+  // Recuperar los gastos totales del mes en curso
+  const totalExpenses = getMonthlyExpenses(transactions);
+
+  const progressPercentage = (totalExpenses / MONEY_TO_EXPENSE) * 100;
+  const progressValue = Math.trunc(Math.min(progressPercentage, 100)); // Asegurarse de que no supere el 100%
+  console.log("Total Expenses: ", progressValue);
+
   return (
     <Card className="flex flex-col justify-between">
       <CardHeader className="flex flex-col gap-3">
         <CardTitle className="text-muted-foreground font-bold">
           Gastos Totales
         </CardTitle>
-        <span className="text-5xl font-bold">4.568€</span>
+        <span className="text-5xl font-bold">{totalExpenses.toFixed(2)}€</span>
       </CardHeader>
       <CardContent className="flex flex-col gap-3">
         <span className="text-muted-foreground text-xl">Budget</span>
-        <Progress value={33} />
+        <Progress value={progressValue} />
       </CardContent>
       <CardFooter>
-        <span className="text-4xl">5.000€</span>
+        <span className="text-4xl">{MONEY_TO_EXPENSE}€</span>
       </CardFooter>
     </Card>
   );
--- lib/helpers/transactions.ts
+++ lib/helpers/transactions.ts
@@ -1,5 +1,11 @@
 import { Transaction } from "../store/useTransactionStore";
 
+/**
+ * @description This function takes an array of transactions and returns the total expenses grouped by month.
+ * It returns an array of objects with the month name and the total expenses for that month.
+ * @param transactions Array of transactions to calculate expenses from.
+ * @returns Array of objects with month and total expenses.
+ */
 export function getExpensesByMonth(transactions: Transaction[]) {
   const expensesByMonth: Record<string, number> = {};
 
@@ -54,9 +60,41 @@ function categorizeTransaction(concepto: string): string {
   return "Otros";
 }
 
+/**
+ *
+ * @param transactions Array of transactions to categorize.
+ * @description This function takes an array of transactions and categorizes each transaction based on its concept.
+ * It adds a new property `categoria` to each transaction object.
+ * @returns Transaction objects with an additional `categoria` property.
+ */
 export function getTransactionsWithCategories(transactions: Transaction[]) {
   return transactions.map((transaction) => {
     const categoria = categorizeTransaction(transaction.concepto);
     return { ...transaction, categoria };
   });
+}
+
+/**
+ *
+ * @param transactions Array of transactions to calculate monthly expenses.
+ * @description This function calculates the total expenses for the current month from a list of transactions.
+ * @returns number - The total expenses for the current month.
+ */
+export function getMonthlyExpenses(transactions: Transaction[]) {
+  return transactions.reduce((acc, transaction) => {
+    // Comprobar si la transaccion es del mes actual
+    const transactionDate = new Date(transaction.fecha_valor);
+    const currentDate = new Date();
+
+    if (
+      transactionDate.getMonth() !== currentDate.getMonth() &&
+      transactionDate.getFullYear() !== currentDate.getFullYear()
+    )
+      return acc;
+
+    // Comprobar si la transaccion es un gasto
+    if (transaction.importe < 0) return acc + Math.abs(transaction.importe);
+
+    return acc;
+  }, 0);
 }
