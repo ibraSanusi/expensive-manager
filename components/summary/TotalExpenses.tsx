6fc4f4e1c580c0f9800f84b47dbde6f6cc53965e
ibraSanusi <isanu227@gmail.com>
2025-06-06 00:03:39 +0200

    feature: editar el importe maximo

--- components/summary/TotalExpenses.tsx
+++ components/summary/TotalExpenses.tsx
@@ -10,20 +10,37 @@ import {
 import { Progress } from "../ui/progress";
 import { useTransactionStore } from "@/lib/store/useTransactionStore";
 import { getCurrentMonthExpenses } from "@/lib";
+import { useState } from "react";
 
-const MONEY_TO_EXPENSE = 1200; // Presupuesto mensual
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { Pencil } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
-/**
- * Componente que muestra los gastos totales del mes actual.
- * Utiliza el store de transacciones para obtener los datos necesarios.
- */
+const DEFAULT_BUDGET = 1200;
+
 export function TotalExpenses() {
-  const { transactions } = useTransactionStore();
-  // Recuperar los gastos totales del mes en curso
-  const totalExpenses = getCurrentMonthExpenses(transactions);
+  const transactions = useTransactionStore((state) => state.transactions);
+  const [budget, setBudget] = useState(DEFAULT_BUDGET);
+  const [budgetInput, setBudgetInput] = useState(DEFAULT_BUDGET);
 
-  const progressPercentage = (totalExpenses / MONEY_TO_EXPENSE) * 100;
-  const progressValue = Math.trunc(Math.min(progressPercentage, 100)); // Asegurarse de que no supere el 100%
+  const totalExpenses = getCurrentMonthExpenses(transactions);
+  const progressPercentage = (totalExpenses / budget) * 100;
+  const progressValue = Math.trunc(Math.min(progressPercentage, 100));
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const numericValue = parseFloat(e.target.value);
+    setBudgetInput(isNaN(numericValue) ? 0 : numericValue);
+  };
 
   return (
     <Card className="flex flex-col justify-between">
@@ -33,12 +50,51 @@ export function TotalExpenses() {
         </CardTitle>
         <span className="text-5xl font-bold">{totalExpenses.toFixed(2)}€</span>
       </CardHeader>
+
       <CardContent className="flex flex-col gap-3">
-        <span className="text-muted-foreground text-xl">Budget</span>
+        <span className="text-muted-foreground text-xl">Presupuesto</span>
         <Progress value={progressValue} />
       </CardContent>
-      <CardFooter>
-        <span className="text-4xl">{MONEY_TO_EXPENSE}€</span>
+
+      <CardFooter className="flex items-center gap-2">
+        <span className="text-4xl">{budget}€</span>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="cursor-pointer">
+              <Pencil />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Selecciona el importe máximo</DialogTitle>
+              <DialogDescription>
+                <Input
+                  className="text-2xl"
+                  type="number"
+                  value={budgetInput}
+                  onChange={handleInputChange}
+                  placeholder="Presupuesto mensual"
+                />
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button onClick={() => setBudget(budgetInput)}>Guardar</Button>
+              </DialogClose>
+              <DialogClose asChild>
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    setBudget(DEFAULT_BUDGET);
+                    setBudgetInput(DEFAULT_BUDGET);
+                  }}
+                >
+                  Restablecer
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </CardFooter>
     </Card>
   );
